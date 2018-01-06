a13047458020709735922a2da3014e43d3d65764
Yifeng Wang <i@ewind.us>
2018-01-06 21:40:36 +0800

    add hello world poc

---
+++ essential-cpp/code/basics/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string user_name;
+    cout << "Enter first name: ";
+    cin >> user_name;
+    cout << "Hello, " << user_name << "!\n";
+    return 0;
+}
