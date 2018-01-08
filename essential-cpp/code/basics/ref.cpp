f4444a1e201466d9c3ad940a07a0ed3c4bac1ce2
Yifeng Wang <i@ewind.us>
2018-01-09 06:49:39 +0800

    add ref code

---
+++ essential-cpp/code/basics/ref.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ival = 1024;
+    int *pi = &ival;
+    int &rval = ival;
+
+    cout << *pi;
+    cout << rval;
+    return 0;
+}
