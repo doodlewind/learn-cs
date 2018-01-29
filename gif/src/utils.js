63a48c175a042f990485302733612b898eb6b49b
Yifeng Wang <i@ewind.us>
2018-01-29 10:23:55 +0800

    feat: add width/height poc

--- gif/src/utils.js
+++ gif/src/utils.js
@@ -3,7 +3,7 @@ import {
   LOGICAL_SCREEN_DESCRIPTOR
 } from './consts'
 
-function getSubstr (gif, begin, end) {
+function readStr (gif, begin, end) {
   let result = ''
   for (let i = begin; i < end; i++) {
     result += String.fromCharCode(gif[i])
@@ -11,16 +11,20 @@ function getSubstr (gif, begin, end) {
   return result
 }
 
+function readWord (gif, cur) {
+  return gif[cur + 1] << 8 | gif[cur]
+}
+
 const rules = {
   [HEADER_BLOCK]: {
     match: (gif, cur) => (
-        gif[cur] === 0x47 && gif[cur + 1] === 0x49 && gif[cur + 2] === 0x46
-      ),
+      gif[cur] === 0x47 && gif[cur + 1] === 0x49 && gif[cur + 2] === 0x46
+    ),
     query: (gif, cur) => ({
       offset: 0,
       size: 6,
       props: {
-        version: getSubstr(gif, cur + 3, cur + 6)
+        version: readStr(gif, cur + 3, cur + 6)
       }
     }),
     next: [LOGICAL_SCREEN_DESCRIPTOR]
@@ -31,7 +35,8 @@ const rules = {
       offset: cur,
       size: 7,
       props: {
-        binary: gif.subarray(cur, cur + 7)
+        width: readWord(gif, cur),
+        height: readWord(gif, cur + 2)
       }
     }),
     next: []
@@ -44,6 +49,11 @@ export function getBlockMeta (gif, cur) {
     const meta = query(gif, cur)
     cur += meta.size
     const newRules = next.map(key => rules[key])
-    console.log(newRules)
+    for (let i = 0; i < newRules.length; i++) {
+      if (newRules[i].match(gif, cur)) {
+        const { props } = newRules[i].query(gif, cur)
+        console.log(props)
+      }
+    }
   }
 }
