f6c5767f7f7e43947bc1052c357fbeffc4c8c9f7
Yifeng Wang <i@ewind.us>
2018-02-05 09:48:39 +0800

    chore: update wasm video config

--- web-video/wasm/main.js
+++ web-video/wasm/main.js
@@ -1,6 +1,17 @@
 /* eslint-env browser */
 const $input = document.querySelector('input')
 const $link = document.getElementById('link')
+const $video = document.getElementById('video')
+
+function arrayBufferToBase64 (buffer) {
+  let binary = ''
+  const bytes = new Uint8Array(buffer)
+  const len = bytes.byteLength
+  for (let i = 0; i < len; i++) {
+    binary += String.fromCharCode(bytes[i])
+  }
+  return window.btoa(binary)
+}
 
 $input.onchange = e => {
   const { files } = $input
@@ -11,5 +22,7 @@ $input.onchange = e => {
     const url = URL.createObjectURL(blob)
     $link.href = url
     $link.hidden = false
+    // FIXME
+    // $video.src = arrayBufferToBase64(e.data)
   })
 }
--- web-video/wasm/worker.js
+++ web-video/wasm/worker.js
@@ -10,10 +10,12 @@ onmessage = function (e) {
     arguments: [
       '-i', '/input/' + files[0].name,
       '-strict', '-2',
+      '-b:v', '2M',
       'out.mp4'
     ],
     files
   }, function (results) {
+    console.log('len: ' + results[0].data.byteLength)
     console.timeEnd('encode')
     self.postMessage(results[0].data)
   })
