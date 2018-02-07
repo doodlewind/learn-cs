30c065fd8f0dc4a7f9d52b51783b5022022491f8
Yifeng Wang <i@ewind.us>
2018-02-07 14:15:27 +0800

    fix: simplify alloc

--- mobile-native/oc/objects/message.m
+++ mobile-native/oc/objects/message.m
@@ -5,17 +5,17 @@
 int main(int argc, const char *argv[]) {
   @autoreleasepool {
     NSDateComponents *comps = [[NSDateComponents alloc] init];
-    [comps setYear:1969];
-    [comps setMonth:4];
-    [comps setDay:30];
-    [comps setHour:13];
-    [comps setMinute:10];
+    [comps setYear:2000];
+    [comps setMonth:1];
+    [comps setDay:1];
+    [comps setHour:0];
+    [comps setMinute:0];
     [comps setSecond:0];
-    NSCalendar *g =
-        [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSCalendar *g = [[NSCalendar alloc]
+        initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     NSDate *dateOfBirth = [g dateFromComponents:comps];
-    double d = (double)[[NSDate date] timeIntervalSinceDate:dateOfBirth];
-    NSLog(@"%f", d);
+    double seconds = [dateOfBirth timeIntervalSince1970];
+    NSLog(@"%f seconds.", seconds);
   }
   return 0;
 }
