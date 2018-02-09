3b15ad2e996dc1091aa7e42494a4956e2c292e12
Yifeng Wang <i@ewind.us>
2018-02-09 18:05:04 +0800

    chore: fix lint

--- mobile-native/VideoEditor/index.js
+++ mobile-native/VideoEditor/index.js
@@ -1,3 +1,3 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('VideoEditor', () => App);
+import { AppRegistry } from 'react-native'
+import App from './App'
+AppRegistry.registerComponent('VideoEditor', () => App)
