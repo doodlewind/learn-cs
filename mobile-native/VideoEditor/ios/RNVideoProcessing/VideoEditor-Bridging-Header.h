a3238640c3cc62376a06d4197c9489a133a04e5e
Yifeng Wang <i@ewind.us>
2018-02-22 12:12:37 +0800

    fix: video header path

--- mobile-native/VideoEditor/ios/RNVideoProcessing/VideoEditor-Bridging-Header.h
+++ mobile-native/VideoEditor/ios/RNVideoProcessing/VideoEditor-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <RCTRootView.h>
-#import <RCTView.h>
-#import "RCTBridgeModule.h"
+#import <React/RCTRootView.h>
+#import <React/RCTView.h>
+#import "React/RCTBridgeModule.h"
 #import "RNVideoProcessing.h"
