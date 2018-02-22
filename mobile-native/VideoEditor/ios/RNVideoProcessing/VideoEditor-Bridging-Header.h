10d9649eefb696ef5aa21c007c13dbe3329b1524
Yifeng Wang <i@ewind.us>
2018-02-22 11:29:44 +0800

    chore: add video processing lib deps

---
+++ mobile-native/VideoEditor/ios/RNVideoProcessing/VideoEditor-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <RCTRootView.h>
+#import <RCTView.h>
+#import "RCTBridgeModule.h"
+#import "RNVideoProcessing.h"
---
+++ mobile-native/VideoEditor/ios/VideoEditor/Placeholder.swift
@@ -0,0 +1,12 @@
+//
+//  Placeholder.swift
+//  VideoEditor
+//
+//  Created by 王译锋 on 2018/2/22.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+// Empty placeholder
+// See https://facebook.github.io/react-native/docs/native-modules-ios.html
+
+import Foundation
