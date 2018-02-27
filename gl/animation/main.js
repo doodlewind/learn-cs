a0938b17a4a62edf0edecd38736c6438cc145776
Yifeng Wang <i@ewind.us>
2018-02-27 19:11:49 +0800

    feat: add animation poc

---
+++ gl/animation/main.js
@@ -0,0 +1,18 @@
+// Inline from https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Using_shaders_to_apply_color_in_WebGL
+// Usage: `parcel index.html`
+
+import { render } from './render'
+import { initBuffers } from './buffers'
+import { initProgram } from './shaders'
+
+// Init WebGL context.
+const gl = document.querySelector('#glcanvas').getContext('webgl')
+
+// Init shader program.
+const programInfo = initProgram(gl)
+
+// Init postion buffer.
+const buffers = initBuffers(gl)
+
+// Render a frame.
+render(gl, programInfo, buffers)
