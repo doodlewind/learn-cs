f22cfe7e745ce5f86fa7edf5036595cc4b27c186
Yifeng Wang <i@ewind.us>
2018-02-27 18:09:43 +0800

    feat: add color shader poc

--- gl/color/buffers.js
+++ gl/color/buffers.js
@@ -20,7 +20,19 @@ export function initBuffers (gl) {
   // JavaScript array, then use it to fill the current buffer.
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
 
+  // Bind color buffer data.
+  const colors = [
+    1.0, 1.0, 1.0, 1.0,   // white
+    1.0, 0.0, 0.0, 1.0,   // red
+    0.0, 1.0, 0.0, 1.0,   // green
+    0.0, 0.0, 1.0, 1.0    // blue
+  ]
+  const colorBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW)
+
   return {
-    position: positionBuffer
+    position: positionBuffer,
+    color: colorBuffer
   }
 }
