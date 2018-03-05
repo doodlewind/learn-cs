673c84f5dbd4d224c231f710eac276b78312f94b
Yifeng Wang <i@ewind.us>
2018-03-05 21:14:43 +0800

    feat: add vue project scaffold

---
+++ video/ui/src/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Hello/>
+  </div>
+</template>
+
+<script>
+import Hello from './components/hello'
+
+export default {
+  name: 'App',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+}
+</style>
